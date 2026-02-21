d8ef88df300ff06eb2dccae2950c72b99331821e
CoffeeCatRailway <dragonlee1002@gmail.com>
2026-02-21 21:05:35 +1100

    Distraction and dt

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 use sdl3::event::{Event, WindowEvent};
 use sdl3::keyboard::Keycode;
 use sdl3::pixels::Color;
-use sdl3::rect::Point;
+use sdl3::rect::{Point, Rect};
 use sdl3::timer;
 
 const FPS: u64 = 60;
@@ -30,8 +30,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let sdl = sdl3::init()?;
 	let video = sdl.video()?;
 	
+	let mut windowSize = Point::new(800, 600);
+	
 	let title = "SDL3 Test";
-	let window = video.window(title, 800, 600)
+	let window = video.window(title, windowSize.x as u32, windowSize.y as u32)
 		.position_centered()
 		.resizable()
 		.build()?;
@@ -39,10 +41,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 	let mut canvas = window.into_canvas();
 	let mut events = sdl.event_pump()?;
 	
-	let mut p = Point::new(800, 600);
+	let mut t = 0.0;
 	
 	let mut fps: u64 = 0;
 	let mut lastTick: u64 = 0;
+	let mut dt: f32 = OPTIMAL_WAIT_TIME as f32 / 1000.0;
 	'main: loop {
 		let startTick = timer::ticks();
 		
@@ -54,8 +57,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 				Event::Window { win_event, ..} => {
 					match win_event {
 						WindowEvent::Resized(width, height) => {
-							p.x = width;
-							p.y = height;
+							windowSize.x = width;
+							windowSize.y = height;
 						},
 						_ => {},
 					}
@@ -65,13 +68,24 @@ fn main() -> Result<(), Box<dyn Error>> {
 		}
 		
 		// update
+		t += dt;
 		
 		// render
 		canvas.set_draw_color(Color::BLACK);
 		canvas.clear();
 		
+		canvas.set_draw_color(Color::WHITE);
+		let s = windowSize.x.min(windowSize.y) / 4;
+		let p = windowSize / 2;
+		let (xo, yo) = {
+			let x = (t * 1.5).sin() * (s as f32 / 2.0);
+			let y = (t * 1.5).cos() * (s as f32 / 2.0);
+			(x as i32, y as i32)
+		};
+		canvas.draw_rect(Rect::new(p.x - s / 2 + xo, p.y - s / 2 + yo, s as u32, s as u32))?;
+		
 		canvas.set_draw_color(Color::GREEN);
-		canvas.draw_line(Point::new(0, 0), p)?;
+		canvas.draw_line(Point::new(0, 0), windowSize)?;
 		
 		canvas.present();
 		
@@ -89,6 +103,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 		// timing
 		let elapsedTicks = timer::ticks() - startTick;
 		let waitTime = OPTIMAL_WAIT_TIME.saturating_sub(elapsedTicks);
+		dt = waitTime as f32 / 1000.0;
 		if waitTime > 0 {
 			// println!("{}", waitTime);
 			timer::delay(waitTime as u32);
